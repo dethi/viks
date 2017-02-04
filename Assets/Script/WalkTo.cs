5ae384189a24be25d472263219aa16aa07f68380
Arthur Lehuen <arthur@lehuen.net>
2017-02-04 19:49:43 +0100

    Simple AI

---
+++ Assets/Script/WalkTo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkTo : MonoBehaviour {
+
+    public Transform goal;
+
+    void Start(){
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
