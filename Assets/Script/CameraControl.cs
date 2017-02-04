80bc263f9f55b9dcb847fe5d2429746ca87d4ba4
Arthur Lehuen <arthur@lehuen.net>
2017-02-04 17:07:50 +0100

    Add simple camera script

---
+++ Assets/Script/CameraControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class CameraControl : MonoBehaviour
+    {
+        public float c_Speed = 10;
+        private Vector3 c_Move;
+
+        private void Start()
+        {
+        }
+
+
+        void Update()
+        {
+            float x = 0;
+            float z = 0;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                x += c_Speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                x -= c_Speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                z += c_Speed * Time.deltaTime;
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                z -= c_Speed * Time.deltaTime;
+            }
+
+            transform.position += new Vector3(x, 0, z);
+        }
+    }
+}
