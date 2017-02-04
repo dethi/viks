2229436d794a741c7470c45b00d164890301b815
Arthur Lehuen <arthur@lehuen.net>
2017-02-05 00:49:41 +0100

    Add first spell

---
+++ Assets/Script/GodPower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodPower : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject fireBallPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                GameObject obj = Instantiate(fireBallPrefab, new Vector3(hit.point.x, 20, hit.point.z), Quaternion.identity) as GameObject;
+                obj.transform.Rotate(new Vector3(90, 0, 0));
+                obj.transform.parent = transform;
+            }
+
+        }
+    }
+}
---
+++ Assets/Script/Kill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col){
+        Debug.Log(col.gameObject.name);
+
+        if (col.gameObject.name.Contains("Warrior")){
+            Animator animator = col.gameObject.GetComponent<Animator>();
+            UnityEngine.AI.NavMeshAgent agent = col.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+            animator.SetBool("Dead", true);
+            agent.Stop();
+            Destroy(col.gameObject, 1);
+        }
+
+
+        Destroy(gameObject);
+    }
+}
